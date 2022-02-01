31c399d3b4089dc2cbd5ae2ef1979f490693b8c0
Ian Sodersjerna <iansodersjerna@gmail.com>
2022-01-31 19:55:49 -0500

    Create glideCog.py

---
+++ cogs/glideCog.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import settings
+import discord
+import requests
+
+
+class glide(commands.Cog):
+    url = 'https://image.sodersjerna.com/image/'
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """Logs that the cog was loaded properly"""
+        settings.logger.info(f"glide cog ready!")
+
+    @commands.command(brief="generate an image")
+    async def gen_image(self, ctx, prompt):
+        """"""
+        settings.logger.info(f"gen image from {ctx.author} : {prompt}")
+        await ctx.channel.send("generating image")
+        myobj = {'un': 'ian',
+                 'pw':'plop',
+                 'prompt':prompt}
+        response = requests.post(self.url, json=myobj)
+        with open('gen.png', 'wb') as f:
+            f.write(response.content)
+        await ctx.channel.send(file=discord.File('gen.png'))
+
+
+def setup(client):
+    client.add_cog(glide(client))
